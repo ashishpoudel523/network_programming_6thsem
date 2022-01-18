1c728f60d4d2e660f78134b51f3b6642c61a7bac
Ashish Poudel <ashishpoudel523@gmail.com>
2022-01-18 09:58:32 +0545

    spam checker

---
+++ src/SpamChecker.java
@@ -0,0 +1,32 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class SpamChecker {
+	public static final String blackhole = "spamhaus.org/sbl";
+	private boolean isSpammer(String ip) {
+		try {
+			InetAddress address =  InetAddress.getByName(ip);
+			byte [] quad =  address.getAddress(); //[124, 56, 44, 33]
+			String query = blackhole;
+			for(byte oc:quad) {
+				int unsignedByte = oc<0?oc+256:oc; //-44+356
+				query = unsignedByte+"."+query;
+				
+			}
+			InetAddress.getByName(query);
+			return true;
+			
+		}catch(UnknownHostException ex)  {
+			return false;
+		}
+	}
+	public static void main(String[] args) {
+		String [] ips = {"207.34.56.23", "34.23.232.22", "145.43.67.123"};
+		SpamChecker sc = new SpamChecker();
+		for(String ip:ips) {
+		System.out.println(sc.isSpammer(ip));
+		}
+
+	}
+
+}
